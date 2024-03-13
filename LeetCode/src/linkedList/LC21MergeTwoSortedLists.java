fc61cf5cb908fe5c49e68ca0ec14fb30806297b7
Alex Kim <ajk8710@nyu.edu>
2024-03-13 18:41:30 -0400

    LC21 Merge Two Sorted Lists

---
+++ LeetCode/src/linkedList/LC21MergeTwoSortedLists.java
@@ -0,0 +1,36 @@
+package linkedList;
+
+public class LC21MergeTwoSortedLists {
+    
+    // Create dummy node. Will return dummy.next as new head.
+    // Create curr node to advance, pointing to dummy. (Advancing dummy will loose original dummy)
+    // Compare list1 & list2, curr.next = smaller. Advance pointers.
+    // Edge case: curr.next = leftover from list1 or list2
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;  // curr pointer to advance
+        
+        while (list1 != null && list2 != null) {  // until one of two lists gets to end
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            
+            curr = curr.next;
+        }
+        
+        if (list1 != null) {  // leftover from one of lists
+            curr.next = list1;
+        }
+        
+        if (list2 != null) {
+            curr.next = list2;
+        }
+        
+        return dummy.next;
+    }
+}
