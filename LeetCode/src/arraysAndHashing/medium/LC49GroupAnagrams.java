68ab98c4f67a3a8c765b8d235ce60a6c132af102
Alex Kim <ajk8710@nyu.edu>
2024-04-09 19:17:31 -0400

    LC49 Group Anagrams: For each string of given List<String>, construct char_to_count map representing anagram. Construct anagram to List<String> map. Return its values.

--- LeetCode/src/arraysAndHashing/medium/LC49GroupAnagrams.java
+++ LeetCode/src/arraysAndHashing/medium/LC49GroupAnagrams.java
@@ -1,6 +1,8 @@
 package arraysAndHashing.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -12,6 +14,7 @@ public class LC49GroupAnagrams {
     // A map: characters to count - represents each anagram.
     // Construct "groupByAnagram" map: anagram to list<String>.
     // Return groupByAnagram.values().
+    // Time: O(m*n) where n is length of input list and m is average length of strings.
     public List<List<String>> groupAnagrams(String[] strs) {
         
         // "groupByAnagram" map: anagram to list<String>
@@ -38,6 +41,39 @@ public class LC49GroupAnagrams {
             }
             else {
                 groupByAnagram.get(map).add(str);
+            }
+        }
+        
+        // Return groupByAnagram.values() which is Collection<List<String>>.
+        return new ArrayList<List<String>>(groupByAnagram.values()); 
+    }
+    
+    
+    // Another way is using ArrayList of length 26 as keys, instead of char_to_count map.
+    // 26 index represents count of each char from a to z.
+    public List<List<String>> groupAnagramsUsingArrayList(String[] strs) {
+        
+        // Instead of HashMap<Character, Integer> as a key,
+        // Using ArrayList<Integer> as a key.
+        HashMap<ArrayList<Integer>, List<String>> groupByAnagram = new HashMap<>();
+        
+        // For each string of given list<String>, construct char_to_count map (anagram).
+        // Update groupByAnagram depending on whether char_to_count already present as a key or not. (This check is not necessary if using ArrayList)
+        for (String str : strs) {
+            ArrayList<Integer> anagram = new ArrayList<>(Arrays.asList(new Integer[26]));
+            Collections.fill(anagram, 0);
+            for (char c : str.toCharArray()) {
+                anagram.set(c - 'a', anagram.get(c - 'a') + 1);
+            }
+            
+            // If char_to_count map is not present, put it as new key-value, otherwise add string for proper key.
+            if (!groupByAnagram.containsKey(anagram)) {  // containsKey, not contains.
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                groupByAnagram.put(anagram, list);
+            }
+            else {
+                groupByAnagram.get(anagram).add(str);
             }
         }
         
